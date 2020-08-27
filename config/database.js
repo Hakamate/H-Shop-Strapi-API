3eed9c5cadb70371e5d2ebe0e46908dd6901767b
Hakamate <thomas.clement@viacesi.fr>
2020-08-27 11:26:27 +0200

    Updated database config

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('ec2-54-166-107-5.compute-1.amazonaws.com'),
         port: env.int(5432),
-        database: env('d71ik5qf7hl6c'),
+        database: env('dqmj5i8oetud9'),
         username: env("hvbbiigebinjkz"),
         password: env('453b9d9f700e583586f7d5517dddac67c423fd3747f0bda41a5c88e27ff42e01'),
       },
